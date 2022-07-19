285f012a09b7ea266cda432ba1f4d6e135d3bdcf
Yiniau <yiranzhouyida@gmail.com>
2022-07-19 13:04:01 +0800

    chore: adjust code

--- src/components/route-container.ts
+++ src/components/route-container.ts
@@ -1,33 +1,33 @@
-import debug from 'debug';
-import { css, html, LitElement, nothing } from 'lit';
-import { customElement, property, query, queryAll, state } from 'lit/decorators.js';
-import { unsafeHTML } from 'lit/directives/unsafe-html.js';
-import { cache } from 'lit/directives/cache.js';
-import { Route } from './route';
+// import debug from 'debug';
+// import { css, html, LitElement, nothing } from 'lit';
+// import { customElement, property, query, queryAll, state } from 'lit/decorators.js';
+// import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+// import { cache } from 'lit/directives/cache.js';
+// import { Route } from './route';
 
 
-@customElement('native-route')
-export class RouteContainer extends LitElement {
+// @customElement('native-route')
+// export class RouteContainer extends LitElement {
 
-	@queryAll('native-route')
-	allRouteNode!: NodeListOf<Route>;
+// 	@queryAll('native-route')
+// 	allRouteNode!: NodeListOf<Route>;
 
-	private _is_match_route() {
-		for (const route of this.allRouteNode) {
-			if (route.isActive()) {
-				return true;
-			}
-		}
-		return false;
-	}
+// 	private _is_match_route() {
+// 		for (const route of this.allRouteNode) {
+// 			if (route.isActive()) {
+// 				return true;
+// 			}
+// 		}
+// 		return false;
+// 	}
 
-	protected render() {
-		return html`
-			<slot @slotchanged></slot>
-		`;
-	}
+// 	protected render() {
+// 		return html`
+// 			<slot @slotchanged></slot>
+// 		`;
+// 	}
 
-	connectedCallback(): void {
+// 	connectedCallback(): void {
 		
-	}
-}
+// 	}
+// }
