ffe1b8acf9dfe9c611d419e4ebe1a6a481188074
Yiniau <yiranzhouyida@gmail.com>
2023-03-20 19:29:10 +0800

    chore: adjust history hook logic

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -9,124 +9,217 @@ export function hook_a_link() {
         }
         e.preventDefault();
 
-        history.pushState(
-          {},
-          target.getAttribute('title') ?? '',
-          href
-        );
+        history.pushState({}, target.getAttribute('title') ?? '', href);
       }
     }
   });
 }
 
-export function hook_history_change(lifecycle?: {
+type historyLifecycle = {
   before?: (
-    type: 'push' | 'replace' | 'back' | 'forward' | 'go',
+    type: 'pushState' | 'replaceState' | 'back' | 'forward' | 'go',
     data: any,
     unused: string,
     url?: string | URL | null | undefined
   ) => void;
   after?: (
-    type: 'push' | 'replace' | 'back' | 'forward' | 'go',
+    type: 'pushState' | 'replaceState' | 'back' | 'forward' | 'go',
     data: any,
     unused: string,
     url?: string | URL | null | undefined
   ) => void;
-  url_adapter?: (url: string | URL | null | undefined) => void;
-}) {
-  const prototype = Reflect.getPrototypeOf(history) as History;
-  
-  const originPushState = prototype.pushState;
-  History.prototype.pushState = function pushState(
-    data: any,
-    unused: string,
-    url?: string | URL | null | undefined
-  ) {
-    let _url = url;
-    _url = lifecycle?.url_adapter?.(_url) ?? url;
-    // console.log('pushState', data, unused, url);
-    lifecycle?.before?.('push', data, unused, _url);
-    originPushState.apply(this, [data, unused, _url]);
-    window.dispatchEvent(
-      new CustomEvent('history:pushState', {
-        detail: {
-          type: 'pushState',
-          data,
-          unused,
-          url: _url,
-        },
-      })
-    );
-    lifecycle?.after?.('push', data, unused, _url);
-  };
-
-  const originReplaceState = prototype.replaceState;
-  History.prototype.replaceState = function replaceState(
-    data: any,
-    unused: string,
-    url?: string | URL | null | undefined
-  ) {
-    // console.log('replaceState', data, unused, url);
-    let _url = url;
-    _url = lifecycle?.url_adapter?.(_url) ?? url;
-    lifecycle?.before?.('replace', data, unused, _url);
-    originReplaceState.apply(this, [data, unused, _url]);
-    window.dispatchEvent(
-      new CustomEvent('history:replaceState', {
-        detail: {
-          type: 'replaceState',
-          data,
-          unused,
-          url: _url,
-        },
-      })
-    );
-    lifecycle?.after?.('replace', data, unused, _url);
-  };
-
-  const originBack = prototype.back;
-  History.prototype.back = function back() {
-    lifecycle?.before?.('back', null, '', null);
-    originBack.apply(this, []);
-    window.dispatchEvent(
-      new CustomEvent('history:back', {
-        detail: {
-          type: 'back',
-        },
-      })
-    );
-    lifecycle?.after?.('back', null, '', null);
-  };
-
-  const originForward = prototype.forward;
-  History.prototype.forward = function forward() {
-    lifecycle?.before?.('forward', null, '', null);
-    originForward.apply(this, []);
-    window.dispatchEvent(
-      new CustomEvent('history:forward', {
-        detail: {
-          type: 'forward',
-        },
-      })
-    );
-    lifecycle?.after?.('forward', null, '', null);
-  };
-  
-  const originGo = prototype.go;
-  History.prototype.go = function go(delta?: number) {
-    lifecycle?.before?.('go', null, '', null);
-    originGo.apply(this, [delta]);
-    window.dispatchEvent(
-      new CustomEvent('history:go', {
-        detail: {
-          type: 'go',
-          delta,
-        },
-      })
-    );
-    lifecycle?.after?.('go', null, '', null);
-  };
+  url_adapter?: (url: string | URL | null | undefined) => string;
 }
+
+function _hook_3_params_func(lifecycle: historyLifecycle, func_name: 'replaceState' | 'pushState') {
+  window.history[func_name] = new Proxy(window.history[func_name], {
+    apply: (
+      target,
+      thisArg,
+      argArray: [any, string, string | URL | null | undefined]
+    ) => {
+      const arg0 = argArray[0];
+      const arg1 = argArray[1];
+      const arg2 = argArray[2];
+
+      let _url = arg2;
+      if (typeof lifecycle?.url_adapter === 'function') {
+        _url = lifecycle.url_adapter(_url);
+      }
+
+      lifecycle?.before?.(func_name, arg0, arg1, _url);
+      const ret = target.apply(thisArg, [arg0, arg1, _url]);
+      lifecycle?.after?.(func_name, arg0, arg1, _url);
+
+      window.dispatchEvent(
+        new CustomEvent(`history:${func_name}`, {
+          detail: {
+            type: func_name,
+            data: arg0,
+            unused: arg1,
+            url: _url,
+          },
+        })
+      );
+
+      return ret;
+    },
+  });
+}
+
+function _hook_0_params_func(lifecycle: historyLifecycle, func_name: 'back' | 'forward') {
+  window.history[func_name] = new Proxy(window.history[func_name], {
+    apply: (
+      target,
+      thisArg,
+    ) => {
+      lifecycle?.before?.(func_name, undefined, '', undefined);
+      const ret = target.apply(thisArg, []);
+      lifecycle?.after?.(func_name, undefined, '', undefined);
+
+      window.dispatchEvent(
+        new CustomEvent(`history:${func_name}`, {
+          detail: {
+            type: func_name,
+          },
+        })
+      );
+
+      return ret;
+    },
+  });
+}
+
+function _hook_1_params_func(lifecycle: historyLifecycle, func_name: 'go') {
+  window.history[func_name] = new Proxy(window.history[func_name], {
+    apply: (
+      target,
+      thisArg,
+      argArray: [number],
+    ) => {
+      lifecycle?.before?.(func_name, argArray[0], '', undefined);
+      const ret = target.apply(thisArg, argArray);
+      lifecycle?.after?.(func_name, argArray[0], '', undefined);
+
+      window.dispatchEvent(
+        new CustomEvent(`history:${func_name}`, {
+          detail: {
+            type: func_name,
+            delta: argArray[0]
+          },
+        })
+      );
+
+      return ret;
+    },
+  });
+}
+
+let hook_history_change_hooked = false;
+export function hook_history_change(lifecycle: historyLifecycle = {}) {
+  if (hook_history_change_hooked) return;
+  hook_history_change_hooked = true;
+  _hook_3_params_func(lifecycle, 'pushState');
+  _hook_3_params_func(lifecycle, 'replaceState');
+  _hook_0_params_func(lifecycle, 'back');
+  _hook_0_params_func(lifecycle, 'forward');
+  _hook_1_params_func(lifecycle, 'go');
+}
+
+// export function hook_history_change(lifecycle?: historyLifecycle) {
+//   const prototype = Reflect.getPrototypeOf(history) as History;
+
+//   const originPushState = prototype.pushState;
+//   History.prototype.pushState = function pushState(
+//     data: any,
+//     unused: string,
+//     url?: string | URL | null | undefined
+//   ) {
+//     let _url = url;
+//     _url = lifecycle?.url_adapter?.(_url) ?? url;
+//     // console.log('pushState', data, unused, url);
+//     lifecycle?.before?.('push', data, unused, _url);
+//     originPushState(data, unused, _url);
+//     window.dispatchEvent(
+//       new CustomEvent('history:pushState', {
+//         detail: {
+//           type: 'pushState',
+//           data,
+//           unused,
+//           url: _url,
+//         },
+//       })
+//     );
+//     lifecycle?.after?.('push', data, unused, _url);
+//   };
+
+//   const originReplaceState = prototype.replaceState;
+//   History.prototype.replaceState = function replaceState(
+//     data: any,
+//     unused: string,
+//     url?: string | URL | null | undefined
+//   ) {
+//     // console.log('replaceState', data, unused, url);
+//     let _url = url;
+//     _url = lifecycle?.url_adapter?.(_url) ?? url;
+//     lifecycle?.before?.('replace', data, unused, _url);
+//     originReplaceState(data, unused, _url);
+//     window.dispatchEvent(
+//       new CustomEvent('history:replaceState', {
+//         detail: {
+//           type: 'replaceState',
+//           data,
+//           unused,
+//           url: _url,
+//         },
+//       })
+//     );
+//     lifecycle?.after?.('replace', data, unused, _url);
+//   };
+
+//   const originBack = prototype.back;
+//   History.prototype.back = function back() {
+//     lifecycle?.before?.('back', null, '', null);
+//     originBack();
+//     window.dispatchEvent(
+//       new CustomEvent('history:back', {
+//         detail: {
+//           type: 'back',
+//         },
+//       })
+//     );
+//     lifecycle?.after?.('back', null, '', null);
+//   };
+
+//   const originForward = prototype.forward;
+//   History.prototype.forward = function forward() {
+//     lifecycle?.before?.('forward', null, '', null);
+//     originForward.apply(this, []);
+//     window.dispatchEvent(
+//       new CustomEvent('history:forward', {
+//         detail: {
+//           type: 'forward',
+//         },
+//       })
+//     );
+//     lifecycle?.after?.('forward', null, '', null);
+//   };
+
+//   const originGo = prototype.go;
+//   History.prototype.go = function go(delta?: number) {
+//     lifecycle?.before?.('go', null, '', null);
+//     originGo.apply(this, [delta]);
+//     window.dispatchEvent(
+//       new CustomEvent('history:go', {
+//         detail: {
+//           type: 'go',
+//           delta,
+//         },
+//       })
+//     );
+//     lifecycle?.after?.('go', null, '', null);
+//   };
+// }
 
 export type HistoryChangeEvent = CustomEvent<{
   data: any;
